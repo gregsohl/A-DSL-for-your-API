6d26780e6c32165269bb8d2434ff6c567f974100
greg <gregsohl@gmail.com>
2017-06-11 10:00:10 -0500

    Implemented shell for all Visit overrides and body of a few
    - AdditiveOpExpression
    - AndExpression
    - CompareExpression
    - IntExpression
    - MultiplicativeOpExpression
    - OrExpression

--- TurtleScript.Interpreter/TurtleScriptInterpreter.cs
+++ TurtleScript.Interpreter/TurtleScriptInterpreter.cs
@@ -49,7 +49,223 @@ namespace TurtleScript.Interpreter
 
 			Visit(parser.script());
 		}
-		#region Private Fields
+
+		public override TurtleScriptValue VisitAdditiveExpression(TurtleScriptParser.AdditiveExpressionContext context)
+		{
+			TurtleScriptValue leftValue = Visit(context.expression(0));
+			TurtleScriptValue rightValue = Visit(context.expression(1));
+
+			TurtleScriptValue result;
+			if (context.op.TokenIndex == TurtleScriptParser.ADD)
+			{
+				result = new TurtleScriptValue(leftValue.NumericValue + rightValue.NumericValue);
+			}
+			else
+			{
+				result = new TurtleScriptValue(leftValue.NumericValue - rightValue.NumericValue);
+			}
+
+			return result;
+		}
+
+		public override TurtleScriptValue VisitAndExpression(TurtleScriptParser.AndExpressionContext context)
+		{
+			TurtleScriptValue leftValue = Visit(context.expression(0));
+			TurtleScriptValue rightValue = Visit(context.expression(1));
+
+			TurtleScriptValue result = new TurtleScriptValue(leftValue.BoolValue && rightValue.BoolValue);
+
+			return result;
+		}
+
+		public override TurtleScriptValue VisitAssignment(TurtleScriptParser.AssignmentContext context)
+		{
+			return base.VisitAssignment(context);
+		}
+
+		public override TurtleScriptValue VisitBlock(TurtleScriptParser.BlockContext context)
+		{
+			return base.VisitBlock(context);
+		}
+
+		public override TurtleScriptValue VisitCompareExpression(TurtleScriptParser.CompareExpressionContext context)
+		{
+			TurtleScriptValue leftValue = Visit(context.expression(0));
+			TurtleScriptValue rightValue = Visit(context.expression(1));
+
+			TurtleScriptValue result;
+
+			switch (context.op.TokenIndex)
+			{
+				case TurtleScriptParser.EQ:
+					result = new TurtleScriptValue(leftValue.NumericValue == rightValue.NumericValue);
+					break;
+				case TurtleScriptParser.NE:
+					result = new TurtleScriptValue(leftValue.NumericValue != rightValue.NumericValue);
+					break;
+				case TurtleScriptParser.GT:
+					result = new TurtleScriptValue(leftValue.NumericValue > rightValue.NumericValue);
+					break;
+				case TurtleScriptParser.LT:
+					result = new TurtleScriptValue(leftValue.NumericValue < rightValue.NumericValue);
+					break;
+				case TurtleScriptParser.GE:
+					result = new TurtleScriptValue(leftValue.NumericValue >= rightValue.NumericValue);
+					break;
+				case TurtleScriptParser.LE:
+					result = new TurtleScriptValue(leftValue.NumericValue <= rightValue.NumericValue);
+					break;
+				default:
+					result = new TurtleScriptValue(false);
+					break;
+			}
+
+			return result;
+		}
+
+		public override TurtleScriptValue VisitElseIfStat(TurtleScriptParser.ElseIfStatContext context)
+		{
+			return base.VisitElseIfStat(context);
+		}
+
+		public override TurtleScriptValue VisitElseStat(TurtleScriptParser.ElseStatContext context)
+		{
+			return base.VisitElseStat(context);
+		}
+
+		public override TurtleScriptValue VisitExpression(TurtleScriptParser.ExpressionContext context)
+		{
+			return base.VisitExpression(context);
+		}
+
+		public override TurtleScriptValue VisitExpressionList(TurtleScriptParser.ExpressionListContext context)
+		{
+			return base.VisitExpressionList(context);
+		}
+
+		public override TurtleScriptValue VisitFloatExpression(TurtleScriptParser.FloatExpressionContext context)
+		{
+			return base.VisitFloatExpression(context);
+		}
+
+		public override TurtleScriptValue VisitFormalParameter(TurtleScriptParser.FormalParameterContext context)
+		{
+			return base.VisitFormalParameter(context);
+		}
+
+		public override TurtleScriptValue VisitFormalParameters(TurtleScriptParser.FormalParametersContext context)
+		{
+			return base.VisitFormalParameters(context);
+		}
+
+		public override TurtleScriptValue VisitForStatement(TurtleScriptParser.ForStatementContext context)
+		{
+			return base.VisitForStatement(context);
+		}
+
+		public override TurtleScriptValue VisitFunctionCall(TurtleScriptParser.FunctionCallContext context)
+		{
+			return base.VisitFunctionCall(context);
+		}
+
+		public override TurtleScriptValue VisitFunctionCallExpression(TurtleScriptParser.FunctionCallExpressionContext context)
+		{
+			return base.VisitFunctionCallExpression(context);
+		}
+
+		public override TurtleScriptValue VisitFunctionDecl(TurtleScriptParser.FunctionDeclContext context)
+		{
+			return base.VisitFunctionDecl(context);
+		}
+
+		public override TurtleScriptValue VisitIfStat(TurtleScriptParser.IfStatContext context)
+		{
+			return base.VisitIfStat(context);
+		}
+
+		public override TurtleScriptValue VisitIfStatement(TurtleScriptParser.IfStatementContext context)
+		{
+			return base.VisitIfStatement(context);
+		}
+
+		public override TurtleScriptValue VisitIntExpression(TurtleScriptParser.IntExpressionContext context)
+		{
+			int value;
+			if (Int32.TryParse(context.GetText(), out value))
+			{
+				return new TurtleScriptValue(value);
+			}
+
+			throw new InvalidOperationException("");
+		}
+
+		public override TurtleScriptValue VisitMultiplicativeOpExpression(TurtleScriptParser.MultiplicativeOpExpressionContext context)
+		{
+			TurtleScriptValue leftValue = Visit(context.expression(0));
+			TurtleScriptValue rightValue = Visit(context.expression(1));
+
+			TurtleScriptValue result;
+			if (context.op.TokenIndex == TurtleScriptParser.MUL)
+			{
+				result = new TurtleScriptValue(leftValue.NumericValue * rightValue.NumericValue);
+			}
+			else
+			{
+				if (rightValue.NumericValue == 0)
+				{
+					result = new TurtleScriptValue(0);
+				}
+				else
+				{
+					result = new TurtleScriptValue(leftValue.NumericValue / rightValue.NumericValue);
+				}
+			}
+
+			return result;
+		}
+
+		public override TurtleScriptValue VisitOrExpression(TurtleScriptParser.OrExpressionContext context)
+		{
+			TurtleScriptValue leftValue = Visit(context.expression(0));
+			TurtleScriptValue rightValue = Visit(context.expression(1));
+
+			TurtleScriptValue result = new TurtleScriptValue(leftValue.BoolValue || rightValue.BoolValue);
+
+			return result;
+		}
+
+		public override TurtleScriptValue VisitParenExpression(TurtleScriptParser.ParenExpressionContext context)
+		{
+			return base.VisitParenExpression(context);
+		}
+
+		public override TurtleScriptValue VisitScript(TurtleScriptParser.ScriptContext context)
+		{
+			return base.VisitScript(context);
+		}
+
+		public override TurtleScriptValue VisitStatement(TurtleScriptParser.StatementContext context)
+		{
+			return base.VisitStatement(context);
+		}
+
+		public override TurtleScriptValue VisitUnaryNegationExpression(TurtleScriptParser.UnaryNegationExpressionContext context)
+		{
+			return base.VisitUnaryNegationExpression(context);
+		}
+
+		public override TurtleScriptValue VisitUnaryNotExpression(TurtleScriptParser.UnaryNotExpressionContext context)
+		{
+			return base.VisitUnaryNotExpression(context);
+		}
+
+		public override TurtleScriptValue VisitVariableReferenceExpression(TurtleScriptParser.VariableReferenceExpressionContext context)
+		{
+			return base.VisitVariableReferenceExpression(context);
+		}
+		#endregion Public Properties
+
+		#region Private Fields
 
 		private readonly string m_Script;
 		private TurtleScriptErrorListener m_TurtleScriptErrorListener;
