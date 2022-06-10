3bf400ae63986cc1a092b9d6e2000721baa4ec96
Greg Sohl <gregsohl@gmail.com>
2022-06-09 20:57:26 -0500

    Added token class hierarchy. Implemented basic tokenizer with values, variable assignment, block and script.

---
+++ TurtleScript.Interpreter/Tokenize/TokenBase.cs
@@ -0,0 +1,126 @@
+﻿#region Namespaces
+
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Antlr4.Runtime;
+
+#endregion Namespaces
+
+namespace TurtleScript.Interpreter.Tokenize
+{
+	public class TokenBase
+	{
+		public TokenBase(TokenType tokenType)
+		{
+			TokenType = tokenType;
+		}
+
+		public List<TokenBase> Children
+		{
+			[DebuggerStepThrough]
+			get { return m_Children; }
+		}
+
+		public TokenType TokenType
+		{
+			[DebuggerStepThrough]
+			get;
+		}
+
+		public void AddChild(TokenBase token)
+		{
+			if (m_Children == null)
+			{
+				m_Children = new List<TokenBase>();
+			}
+
+			m_Children.Add(token);
+		}
+
+		public string ToTurtleScript()
+		{
+			return string.Empty;
+		}
+
+		private List<TokenBase> m_Children;
+	}
+
+	public class TokenValue : TokenBase
+	{
+		public TokenValue(TokenType tokenType)
+			: base(tokenType)
+		{
+		}
+	}
+
+	public class TokenNullValue : TokenValue
+	{
+		public TokenNullValue()
+			: base(TokenType.NullValue)
+		{
+		}
+	}
+
+	public class TokenBooleanValue : TokenValue
+	{
+		public TokenBooleanValue(bool value)
+			: base(TokenType.Boolean)
+		{
+			Value = value;
+		}
+
+		public bool Value
+		{
+			[DebuggerStepThrough]
+			get;
+		}
+	}
+
+	public class TokenNumericValue : TokenValue
+	{
+		public TokenNumericValue(double value)
+			: base(TokenType.Numeric)
+		{
+			Value = value;
+		}
+
+		public double Value
+		{
+			[DebuggerStepThrough]
+			get;
+		}
+	}
+
+	public class TokenBinaryOperator : TokenBase
+	{
+		public TokenBinaryOperator(TokenType tokenType)
+			: base(tokenType)
+		{
+		}
+	}
+
+	public class TokenAssignment : TokenBase
+	{
+		public TokenAssignment(string variableName)
+			: base(TokenType.Assignment)
+		{
+			m_VariableName = variableName;
+		}
+
+		private string m_VariableName;
+	}
+
+	public enum TokenType
+	{
+		Script,
+		Block,
+		Boolean,
+		Numeric,
+		NullValue,
+		Assignment,
+		Add,
+		Subtrract,
+
+	}
+}
---
+++ TurtleScript.Interpreter/Tokenize/TurtleScriptTokenizer.cs
@@ -0,0 +1,191 @@
+#region Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+using TurtleScript.Interpreter.Tokenize;
+
+#endregion Namespaces
+
+namespace TurtleScript.Interpreter.Tokenize
+{
+	public class TurtleScriptTokenizer
+		: TurtleScriptBaseVisitor<TokenBase>
+	{
+		#region Constructor
+		public TurtleScriptTokenizer(
+			string script, 
+			List<ITurtleScriptRuntime> runtimeLibraries = null)
+		{
+			m_Script = script;
+			m_Tokens = new List<TokenBase>();
+
+			m_RuntimeLibraries = runtimeLibraries ?? new List<ITurtleScriptRuntime>();
+
+			m_TurtleScriptErrorListener = new TurtleScriptErrorListener();
+			m_Variables = new Dictionary<string, TokenBase>();
+			m_ScriptFunctions = new Dictionary<string, TurtleScriptFunction>();
+		}
+
+		#endregion Constructor
+
+		#region Public Properties
+
+		public string ErrorMessage
+		{
+			get
+			{
+				if (!string.IsNullOrEmpty(m_ErrorMessage))
+				{
+					return m_ErrorMessage;
+				}
+
+				return m_TurtleScriptErrorListener.Message;
+			}
+		}
+
+		public bool IsError
+		{
+			get
+			{
+				return
+				(
+					(!string.IsNullOrEmpty(m_TurtleScriptErrorListener.Message)) ||
+					(!string.IsNullOrEmpty(m_ErrorMessage))
+				);
+			}
+		}
+
+		public Dictionary<string, TokenBase> Variables
+		{
+			get { return m_Variables; }
+		}
+
+		#endregion Public Properties
+
+		#region Public Methods
+
+		/// <summary>
+		/// Executes the script
+		/// </summary>
+		/// <returns><c>true</c> if execution is successful, otherwise <c>false</c></returns>
+		public bool Execute()
+		{
+			AntlrInputStream input = new AntlrInputStream(m_Script);
+
+			TurtleScriptLexer lexer = new TurtleScriptLexer(input);
+
+			CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+
+			TurtleScriptParser parser = new TurtleScriptParser(tokenStream);
+
+			parser.RemoveErrorListeners();
+			m_TurtleScriptErrorListener = new TurtleScriptErrorListener();
+			parser.AddErrorListener(m_TurtleScriptErrorListener);
+
+			parser.BuildParseTree = true;
+
+			m_Variables = new Dictionary<string, TokenBase>();
+			m_ScriptFunctions = new Dictionary<string, TurtleScriptFunction>();
+
+			try
+			{
+				TokenBase turtleScriptValue = Visit(parser.script());
+			}
+			catch (InvalidOperationException exception)
+			{
+				m_ErrorMessage = exception.Message;
+			}
+
+			return !IsError;
+		}
+
+		public override TokenBase VisitAssignment(TurtleScriptParser.AssignmentContext context)
+		{
+			TokenBase value = Visit(context.expression());
+
+			TokenBase result = new TokenAssignment(context.Identifier().GetText());
+			result.AddChild(value);
+
+			return result;
+		}
+
+		public override TokenBase VisitAdditiveExpression(TurtleScriptParser.AdditiveExpressionContext context)
+		{
+			TokenBase leftValue = Visit(context.expression(0));
+			TokenBase rightValue = Visit(context.expression(1));
+
+			TokenBase result = new TokenBinaryOperator(
+				context.op.Type == TurtleScriptParser.Add ? TokenType.Add : TokenType.Subtrract);
+
+			result.AddChild(leftValue);
+			result.AddChild(rightValue);
+
+			return result;
+		}
+
+		public override TokenBase VisitFloatExpression(TurtleScriptParser.FloatExpressionContext context)
+		{
+			if (double.TryParse(context.GetText(), out var value))
+			{
+				return new TokenNumericValue(value);
+			}
+
+			throw new InvalidOperationException($"Invalid numeric value. Line {context.Start.Line}, Column {context.Start.Column}"); 
+		}
+
+
+		public override TokenBase VisitIntExpression(TurtleScriptParser.IntExpressionContext context)
+		{
+			if (Int32.TryParse(context.GetText(), out var value))
+			{
+				return new TokenNumericValue(value);
+			}
+
+			throw new InvalidOperationException("Invalid integer value");
+		}
+
+		public override TokenBase VisitScript(TurtleScriptParser.ScriptContext context)
+		{
+			TokenBase scriptToken = new TokenBase(TokenType.Script);
+
+			scriptToken.AddChild(Visit(context.block()));
+
+			return scriptToken;
+		}
+
+		public override TokenBase VisitBlock(TurtleScriptParser.BlockContext context)
+		{
+			TokenBase blockToken = new TokenBase(TokenType.Block);
+
+			foreach (TurtleScriptParser.StatementContext statementContext in context.statement())
+			{
+				blockToken.AddChild(Visit(statementContext));
+			}
+
+			return blockToken;
+		}
+
+		#endregion Public Methods
+
+		#region Private Fields
+
+		private readonly string m_Script;
+		private readonly List<ITurtleScriptRuntime> m_RuntimeLibraries;
+		private TurtleScriptErrorListener m_TurtleScriptErrorListener;
+		private string m_ErrorMessage;
+		private Dictionary<string, TokenBase> m_Variables;
+		private Dictionary<string, TurtleScriptFunction> m_ScriptFunctions;
+		private List<TokenBase> m_Tokens;
+
+		#endregion Private Fields
+
+		#region Private Methods
+
+		#endregion Private Methods
+
+	}
+}
