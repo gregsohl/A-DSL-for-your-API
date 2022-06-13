9441e2201fb1cf683323c187d19ea4749bb46b5a
Greg Sohl <gregsohl@gmail.com>
2022-06-13 11:51:56 -0500

    Added tests for pi constant in parser visitor interpreter

---
+++ TurtleScript.Interpreter.UnitTest/PredefinedConstantsTests.cs
@@ -0,0 +1,53 @@
+﻿#region Namespaces
+
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+#endregion Namespaces
+
+namespace TurtleScript.Interpreter.UnitTest
+{
+	public class PredefinedConstantsTests
+	{
+		private const double PI = 3.141592654;
+
+		[Test]
+		public void Pi()
+		{
+			// Arrange
+			var script = "a = pi";
+
+			TurtleScriptInterpreter interpreter = new TurtleScriptInterpreter(script);
+
+			// Act
+			bool success = interpreter.Execute();
+
+			// Assert
+			Assert.IsTrue(success, interpreter.ErrorMessage);
+
+			TurtleScriptValue variableValue = interpreter.Variables["a"];
+			Assert.AreEqual(PI, variableValue.NumericValue);
+		}
+
+		[Test]
+		public void PiMath()
+		{
+			// Arrange
+			var script = "a = pi * 2";
+
+			TurtleScriptInterpreter interpreter = new TurtleScriptInterpreter(script);
+
+			// Act
+			bool success = interpreter.Execute();
+
+			// Assert
+			Assert.IsTrue(success, interpreter.ErrorMessage);
+
+			TurtleScriptValue variableValue = interpreter.Variables["a"];
+			Assert.AreEqual(PI * 2, variableValue.NumericValue);
+		}
+
+
+	}
+}
