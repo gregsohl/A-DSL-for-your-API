261c47d5d919b53591320a5514c4d8522015c7eb
Greg Sohl <gregsohl@gmail.com>
2022-07-28 23:02:08 -0500

    Moved Git repo up two folders so as to include the presentation and other misc files.

---
+++ source/TurtleScript/ScriptedTurtle/Runtime/TurtleMath.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TurtleScript.Interpreter;
+
+namespace ScriptedTurtle.Runtime
+{
+	public class TurtleMath : ITurtleScriptRuntime
+	{
+		public TurtleMath()
+		{
+			m_Functions = new Dictionary<string, TurtleScriptRuntimeFunction>();
+
+			m_Functions.Add("sin_1", new TurtleScriptRuntimeFunction(Sin, 1));
+			m_Functions.Add("cos_1", new TurtleScriptRuntimeFunction(Cos, 1));
+			m_Functions.Add("tan_1", new TurtleScriptRuntimeFunction(Tan, 1));
+		}
+
+		public string Namespace
+		{
+			get { return "m"; }
+		}
+
+		public Dictionary<string, TurtleScriptRuntimeFunction> Functions
+		{
+			get { return m_Functions; }
+		}
+
+		public TurtleScriptValue Sin(List<TurtleScriptValue> parameters)
+		{
+			double angle = parameters[0].NumericValue;
+			return new TurtleScriptValue(Math.Sin(angle));
+		}
+
+		public TurtleScriptValue Cos(List<TurtleScriptValue> parameters)
+		{
+			double angle = parameters[0].NumericValue;
+			return new TurtleScriptValue(Math.Cos(angle));
+		}
+
+		public TurtleScriptValue Tan(List<TurtleScriptValue> parameters)
+		{
+			double angle = parameters[0].NumericValue;
+			return new TurtleScriptValue(Math.Tan(angle));
+		}
+
+		#region Private Fields
+
+		private readonly Dictionary<string, TurtleScriptRuntimeFunction> m_Functions;
+
+		#endregion Private Fields
+
+	}
+}
